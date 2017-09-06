1ae9183ff0bb7e0df3cdcbf2ad660697431461f8
Jessa Witzel <jessa@riotousliving.com>
2017-09-06 14:08:33 -0400

    with comments

--- python3.6/ex30.py
+++ python3.6/ex30.py
@@ -1,11 +1,14 @@
+#setting variables
 people = 30
 cars = 40
 trucks = 15
 
 if cars > people:
     print("We should take the cars.")
+#else if statement so if the first statement is false, it moves on.
 elif cars < people:
     print("We should not take the cars.")
+#else signals that if all other statements are not true, do this
 else:
     print("We can't decide.")
 

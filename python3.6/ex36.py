05dc67fef7ac29a2366e8962ef64383f7915e648
Jessa Witzel <jessa@riotousliving.com>
2017-09-13 23:44:11 -0400

    loop through

--- python3.6/ex36.py
+++ python3.6/ex36.py
@@ -13,18 +13,25 @@ keys = list(txtfile.keys())
 random.shuffle(keys)
 
 def vocab(txtfile):
-    print(keys)
-    #pop off tuple into a variable
-    current = keys.pop()
-    print(f"Your vocab word is {current}.")
-    #give the vocab word and request input for the definition
-    whatever = input("Please type your definition: ")
-    #show the definition and asked if it matched
-    print("Here is the definition we have on file:")
-    print(txtfile[current])
-    print("Did you answer correctly?")
+    while keys:
+        #pop off tuple into a variable
+        current = keys.pop()
+        print(f"Your vocab word is {current}.")
+        #give the vocab word and request input for the definition
+        whatever = input("Please type your definition: ")
+        #show the definition and asked if it matched
+        print("Here is the definition we have on file:")
+        print(txtfile[current])
+        print("Did you answer correctly?")
     #if yes, move to next one, if no .amend back onto the list.
-    response = input("> ")
+        response = input("> ")
+        if response == "no":
+            print("That's too bad! We will retest you on this later.")
+            keys.insert(0, current)
+            print(keys)
+        else:
+            print("Great job! We won't test you on that again.")
+
 
 def begin(choice):
     if choice == "vocab":

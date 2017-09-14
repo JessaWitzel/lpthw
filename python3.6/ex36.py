9e749e96fd87b72c84d5ed8345698490c2ecd4f2
Jessa Witzel <jessa@riotousliving.com>
2017-09-14 14:26:15 -0400

    adds comments to plan for next steps

--- python3.6/ex36.py
+++ python3.6/ex36.py
@@ -12,28 +12,44 @@ txtfile = {"word": "definition", "vocab": "iscool", "jessa": "isawesome"}
 keys = list(txtfile.keys())
 random.shuffle(keys)
 
+def guess_vocab(txtfile, current):
+    print(f"Your vocab word is {current}.")
+    whatever = input("Please type your definition: ")
+    print("Here is the definition we have on file:")
+    print(txtfile[current])
+    print("Did you answer correctly?")
+
+def correct_or_naw(current):
+    response = input("> ")
+    if response == "no":
+        print("That's too bad! We will retest you on this later.")
+        keys.insert(0, current)
+        print(keys)
+    else:
+        print("Great job! We won't test you on that again.")
+
+
 def vocab(txtfile):
     while keys:
-        #pop off tuple into a variable
+    #pop off tuple into a variable
         current = keys.pop()
-        print(f"Your vocab word is {current}.")
-        #give the vocab word and request input for the definition
-        whatever = input("Please type your definition: ")
-        #show the definition and asked if it matched
-        print("Here is the definition we have on file:")
-        print(txtfile[current])
-        print("Did you answer correctly?")
+        #print(f"Your vocab word is {current}.")
+    #give the vocab word and request input for the definition
+        #whatever = input("Please type your definition: ")
+    #show the definition and asked if it matched
+        #print("Here is the definition we have on file:")
+        #print(txtfile[current])
+        #print("Did you answer correctly?")
     #if yes, move to next one, if no .insert in beginning of list.
-        response = input("> ")
-        if response == "no":
-            print("That's too bad! We will retest you on this later.")
-            keys.insert(0, current)
-            print(keys)
-        else:
-            print("Great job! We won't test you on that again.")
+        #response = input("> ")
+        #if response == "no":
+            #print("That's too bad! We will retest you on this later.")
+            #keys.insert(0, current)
+            #print(keys)
+        #else:
+            #print("Great job! We won't test you on that again.")
 
 def definitions(txtfile):
-    #copy vocab file but call the definition first and then the keys
     while keys:
         current = keys.pop()
         print(f"Please give us the vocab word that matches this definitions: {txtfile[current]}")
@@ -47,6 +63,12 @@ def definitions(txtfile):
             print(keys)
         else:
             print("Great job! We won't test you on that again.")
+
+def mixture(txtfile):
+    #I want to be able to call the vocab and definitions function inside of this function
+    #separate the call and response and input from the while loop in the functions?
+    #then call that within each function?
+
 
 def begin(choice):
     if choice == "vocab":

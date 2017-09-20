8ee1db9e8fce4518f48187ba10fcb8140a554ef1
Jessa Witzel <jessa@riotousliving.com>
2017-09-20 14:13:07 -0400

    edit functions to reduce repeating code

--- python3.6/ex36.py
+++ python3.6/ex36.py
@@ -14,10 +14,15 @@ random.shuffle(keys)
 
 def guess_vocab(txtfile, current):
     print(f"Your vocab word is {current}.")
-    whatever = input("Please type your definition: ")
+    input("Please type your definition: ")
     print("Here is the definition we have on file:")
     print(txtfile[current])
     print("Did you answer correctly?")
+
+def guess_definitions(txtfile, current):
+    print(f"Please give us the vocab word that matches this definitions: {txtfile[current]}")
+    whatever = input("> ")
+    print(f"The correct vocab word is: {current}")
 
 def correct_or_naw(current):
     response = input("> ")
@@ -31,46 +36,21 @@ def correct_or_naw(current):
 
 def vocab(txtfile):
     while keys:
-    #pop off tuple into a variable
         current = keys.pop()
-        #print(f"Your vocab word is {current}.")
-    #give the vocab word and request input for the definition
-        #whatever = input("Please type your definition: ")
-    #show the definition and asked if it matched
-        #print("Here is the definition we have on file:")
-        #print(txtfile[current])
-        #print("Did you answer correctly?")
-    #if yes, move to next one, if no .insert in beginning of list.
-        #response = input("> ")
-        #if response == "no":
-            #print("That's too bad! We will retest you on this later.")
-            #keys.insert(0, current)
-            #print(keys)
-        #else:
-            #print("Great job! We won't test you on that again.")
         guess_vocab(txtfile, current)
         correct_or_naw(current)
 
 def definitions(txtfile):
     while keys:
         current = keys.pop()
-        print(f"Please give us the vocab word that matches this definitions: {txtfile[current]}")
-        whatever = input("> ")
-        print(f"The correct vocab word is: {current}")
-        print("Did you answer correctly?")
-        response = input("> ")
-        if response == "no":
-            print("That's too bad! We will retest you on this later.")
-            keys.insert(0, current)
-            print(keys)
-        else:
-            print("Great job! We won't test you on that again.")
+        guess_definitions(txtfile, current)
+        correct_or_naw(current)
 
 def mixture(txtfile):
     #I want to be able to call the vocab and definitions function inside of this function
     #separate the call and response and input from the while loop in the functions?
     #then call that within each function?
-
+    print("example text")
 
 def begin(choice):
     if choice == "vocab":

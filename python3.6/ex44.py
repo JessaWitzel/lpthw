9504e465cc3d48b68e2e2c12d9983b63d4b25f30
Jessa Witzel <jessa@riotousliving.com>
2017-09-22 15:16:22 -0400

    Added exercise on composition instead of inheritance

--- python3.6/ex44.py
+++ python3.6/ex44.py
@@ -1,3 +1,4 @@
+##INHERITANCE
 class Parent(object):
 
     def implicit(self):
@@ -32,3 +33,38 @@ dad.altered()
 son.altered()
 
 #question for Wayne regarding how the SUPER runs because the output prints "Parent altered" then "Child Before" then "parent altered" then "Child after"
+
+
+## COMPOSITION
+class Other(object):
+
+    def override(self):
+        print("OTHER override()")
+
+    def implicit(self):
+        print("OTHER implicit()")
+
+    def altered(self):
+        print("OTHER altered()")
+
+class Child(object):
+
+    def __init__(self):
+        self.other = Other()
+
+    def implicit(self):
+        self.other.implicit()
+
+    def override(self):
+        print("CHILD override()")
+
+    def altered(self):
+        print("CHILD, BEFORE OTHER altered")
+        self.other.altered()
+        print("CHILD, AFTER OTHER altered()")
+
+son = Child()
+
+son.implicit()
+son.override()
+son.altered()

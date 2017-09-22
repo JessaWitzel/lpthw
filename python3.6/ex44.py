16caa27371ad75f9fc02187b9568d5f2a1bd851f
Jessa Witzel <jessa@riotousliving.com>
2017-09-22 14:49:13 -0400

    how to override inheritance

--- python3.6/ex44.py
+++ python3.6/ex44.py
@@ -3,13 +3,19 @@ class Parent(object):
     def implicit(self):
         print("PARENT implicit()")
 
+    def override(self):
+        print("PARENT override()")
+
 class Child(Parent):
-    pass
+    def override(self):
+        print("CHILD override()")
 
 dad = Parent()
 son = Child()
-
+#all of the functions/attributes of the parent are present in the child.
 dad.implicit()
 son.implicit()
 
-#all of the functions/attributes of the parent are present in the child.
+#to override a parent class function just define a function with the same name in Child
+dad.override()
+son.override()

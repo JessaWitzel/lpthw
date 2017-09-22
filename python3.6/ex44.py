9aa9f0fb2dddbc454ec54ec04fc3c713adbc22b5
Jessa Witzel <jessa@riotousliving.com>
2017-09-22 14:43:40 -0400

    new exercise on inheritance

---
+++ python3.6/ex44.py
@@ -0,0 +1,15 @@
+class Parent(object):
+
+    def implicit(self):
+        print("PARENT implicit()")
+
+class Child(Parent):
+    pass
+
+dad = Parent()
+son = Child()
+
+dad.implicit()
+son.implicit()
+
+#all of the functions/attributes of the parent are present in the child.

482b6e4f4f3a096f2d22f4bbe68bde2756a69a55
Jessa Witzel <jessa@riotousliving.com>
2017-09-25 12:34:31 -0400

    runs with any text file you designate

--- python3.6/ex36.py
+++ python3.6/ex36.py
@@ -1,40 +1,39 @@
 #I don't want to write a game because I'm not that into video games.
 #input text file as a command line argument
 import random
-vocab = {}
+vocabdict = {}
 
-def create_vocab_dict():
-    vocabtxt = open("examplevocab.txt")
+def create_vocab_dict(txtfile):
+    vocabtxt = open(txtfile)
     nextvocab = vocabtxt.readline().strip("\n")
+    print(nextvocab)
     while nextvocab != "":
-        #strip newline out of it
-
         key, value = nextvocab.split(", ", 1)
-        vocab[key] = value
-        print(vocab)
+        vocabdict[key] = value
         nextvocab = vocabtxt.readline().strip("\n")
 
-create_vocab_dict()
-
 print("""Welcome to the vocab game!
-    Would you like to be shown the vocab,
-    the definitions, or a mixture of both?
-    """)
-
+        Please enter the text file that contains your vocabulary.
+        Vocab words should be separated from their definitions by a comma.
+        """)
+txtfile = input("> ")
+create_vocab_dict(txtfile)
+print("""Would you like to be shown the vocab,
+        the definitions, or a mixture of both?
+        """)
 choice = input("> ")
-txtfile = {"word": "definition", "vocab": "iscool", "jessa": "isawesome"}
-keys = list(txtfile.keys())
+keys = list(vocabdict.keys())
 random.shuffle(keys)
 
-def guess_vocab(txtfile, current):
+def guess_vocab(current):
     print(f"Your vocab word is {current}.")
     input("Please type your definition: ")
     print("Here is the definition we have on file:")
-    print(txtfile[current])
+    print(vocabdict[current])
     print("Did you answer correctly?")
 
-def guess_definitions(txtfile, current):
-    print(f"Please give us the vocab word that matches this definition: {txtfile[current]}")
+def guess_definitions(current):
+    print(f"Please give us the vocab word that matches this definition: {vocabdict[current]}")
     input("> ")
     print(f"The correct vocab word is: {current}")
     print("Did you answer correctly?")
@@ -44,7 +43,6 @@ def correct_or_naw(current):
     if response == "no":
         print("That's too bad! We will retest you on this later.")
         keys.insert(0, current)
-        print(keys)
     else:
         print("Great job! We won't test you on that again.")
 
@@ -52,13 +50,13 @@ def correct_or_naw(current):
 def vocab(txtfile):
     while keys:
         current = keys.pop()
-        guess_vocab(txtfile, current)
+        guess_vocab(current)
         correct_or_naw(current)
 
 def definitions(txtfile):
     while keys:
         current = keys.pop()
-        guess_definitions(txtfile, current)
+        guess_definitions(current)
         correct_or_naw(current)
 
 def mixture(txtfile):
@@ -74,11 +72,11 @@ def mixture(txtfile):
 def begin(choice):
     if choice == "vocab":
         print("Let's do this")
-        vocab(txtfile)
+        vocab(vocabdict)
     elif choice == "definitions":
-        definitions(txtfile)
+        definitions(vocabdict)
     elif "mixture" in choice:
-        mixture(txtfile)
+        mixture(vocabdict)
     else:
         print("Please type 'vocab', 'definitions', or 'mixture'")
         choice = input("> ")
